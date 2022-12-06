a4e0167feb8cdfdfa3755b3961030616b545b7fa
Noelle Khazoum <kh.noelle@gmail.com>
2022-12-06 12:45:20 +0100

    actual prices + pred

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,6 +1,64 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
+####################
+#   Actual prices  #
+####################
+
+st.set_page_config(page_icon="📈", page_title="cryPredict")
+st.image(
+    "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/z3ahdkytzwi1jxlpazje",
+    width=50,
+)
+c1, c2 = st.columns([1, 8])
+with c1:
+    st.image(
+        "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/chart-increasing_1f4c8.png",
+        width=90,
+    )
+st.markdown(
+    """# **Crypto Dashboard**
+A simple cryptocurrency price app pulling price data from the [Binance API](https://www.binance.com/en/support/faq/360002502072).
+"""
+)
+st.header("**Selected Price**")
+
+# Load market data from Binance API
+df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
+
+crpytoList = {
+    "Price 1": "BTCBUSD",
+    "Price 2": "ETHBUSD",
+    "Price 3": "BNBBUSD",
+    "Price 4": "XRPBUSD",
+    "Price 5": "ADABUSD",
+
+}
+
+col1, col2, col3 = st.columns(3)
+
+for i in range(len(crpytoList.keys())):
+    selected_crypto_label = list(crpytoList.keys())[i]
+    selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
+    selected_crypto = st.selectbox(
+        selected_crypto_label, df.symbol, selected_crypto_index, key=str(i)
+    )
+    col_df = df[df.symbol == selected_crypto]
+    col_price = (col_df.weightedAvgPrice)
+    col_percent = f"{float(col_df.priceChangePercent)}%"
+    if i < 3:
+        with col1:
+            st.metric(selected_crypto, col_price, col_percent)
+    if 2 < i < 6:
+        with col2:
+            st.metric(selected_crypto, col_price, col_percent)
+    if i > 5:
+        with col3:
+            st.metric(selected_crypto, col_price, col_percent)
+####################
+#   Prediction  #
+####################
 
 #test branche
 
@@ -10,9 +68,9 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://img.freepik.com/premium-vector/abstract-background-technology-style-white-low-poly-connection-with-nodes-global-data-blockchain-plexus-future-perspective-backdrop-vector-illustration_78474-1029.jpg");
-             background-attachment: fixed;
-             background-size: cover
+
+             color: #01a781;
+             backgroundColor: #01a781
          }}
          </style>
          """,
@@ -20,9 +78,10 @@ def add_bg_from_url():
      )
 add_bg_from_url()
 
+
 #add title
 
-st.title("Hello Team Crypto")
+st.title('Crypto')
 
 
 #add head
@@ -44,7 +103,12 @@ add_bg_from_url()
 st.header("What do you have to do with those one...")
 
 result=st.button("Predict")
+wagon_cab_api_url = 'https://cryptogcloud-wfw7tigrma-ew.a.run.app/predict'
+response = requests.get(wagon_cab_api_url)
 
+prediction = response.json()['prediction']
+#pred = prediction["prediction"]
+st.markdown(prediction)
 st.header("list what to do Keep/Sell/Rebuy..")
 ##list what to do Keep/Sell/Rebuy
 
