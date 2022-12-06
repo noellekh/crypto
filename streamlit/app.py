fb2023239968f8b4c8cdbb3120ab2efa83b12b3f
Noelle Khazoum <kh.noelle@gmail.com>
2022-12-06 16:27:31 +0100

    streamlit tweets

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,27 +2,29 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import streamlit.components.v1 as components
+from transformers import pipeline
+import tweepy as tw
+import os
+
+
 ####################
 #   Actual prices  #
 ####################
 
 st.set_page_config(page_icon="📈", page_title="cryPredict")
-st.image(
-    "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/z3ahdkytzwi1jxlpazje",
-    width=50,
-)
+big_title = '<p style="font-family:Courier; color:#01a781; font-size: 80px;">cryPredict</p>'
+st.markdown(big_title, unsafe_allow_html=True)
+#st.title("cryPredict")
 c1, c2 = st.columns([1, 8])
-with c1:
-    st.image(
-        "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/chart-increasing_1f4c8.png",
-        width=90,
-    )
+
+
 st.markdown(
-    """# **Crypto Dashboard**
-A simple cryptocurrency price app pulling price data from the [Binance API](https://www.binance.com/en/support/faq/360002502072).
+    """# **Crypto actual prices**
+From the [Binance API](https://www.binance.com/en/support/faq/360002502072).
 """
 )
-st.header("**Selected Price**")
+#st.header("**Selected Price**")
 
 # Load market data from Binance API
 df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
@@ -53,9 +55,55 @@ for i in range(len(crpytoList.keys())):
     if 2 < i < 6:
         with col2:
             st.metric(selected_crypto, col_price, col_percent)
-    if i > 5:
-        with col3:
-            st.metric(selected_crypto, col_price, col_percent)
+
+####################
+#   Twitter #
+####################
+def theTweet(tweet_url):
+    api = "https://publish.twitter.com/oembed?url={}".format(tweet_url)
+    response = requests.get(api)
+    res = response.json()["html"]
+    components.html(res,height= 700)
+    return res
+input = st.text_input("Enter your tweet url")
+if input:
+    res = theTweet(input)
+    st.write(res)
+
+#######################
+#   Twitter Sentiment #
+#######################
+
+consumer_key = os.environ.get("api_key")
+print(consumer_key)
+consumer_secret = os.environ.get("api_key_secret")
+access_token = os.environ.get("access_token")
+access_token_secret = os.environ.get("access_token_secret")
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+classifier = pipeline('sentiment-analysis')
+st.title('Live Twitter Sentiment Analysis with Tweepy and HuggingFace Transformers')
+st.markdown('This app uses tweepy to get tweets from twitter based on the input name/phrase. It then processes the tweets through HuggingFace transformers pipeline function for sentiment analysis. The resulting sentiments and corresponding tweets are then put in a dataframe for display which is what you see as result.')
+
+def run():
+    with st.form(key="Enter name"):
+        search_words = st.text_input("Enter the name for which you want to know the sentiment")
+        number_of_tweets = st.number_input("Enter the number of latest tweets for which you want to know the sentiment(Maximum 50 tweets)", 0,50,10)
+        submit_button = st.form_submit_button(label="Submit")
+        if submit_button:
+            tweets =tw.Cursor(api.search_tweets,q=search_words,lang="en").items(number_of_tweets)
+            tweet_list = [i.text for i in tweets]
+        p = [i for i in classifier(tweet_list)]
+        q=[p[i]["label"] for i in range(len(p))]
+        df = pd.DataFrame(list(zip(tweet_list, q)),columns =["Latest "+str(number_of_tweets)+" Tweets"+" on "+search_words, "sentiment"])
+        st.write(df)
+
+
+if __name__=="__main__":
+    run()
+
 ####################
 #   Prediction  #
 ####################
